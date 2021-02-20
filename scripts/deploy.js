13c9c3049f284cde4dc6b93f0974f6e36df5a2e2
Simon Emanuel Schmid <simon@unchained.shop>
2021-02-20 11:10:30 +0000

    Use preset for token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,10 @@
 async function main() {
   const WhaleToken = await global.ethers.getContractFactory('WhaleToken');
-  const whaleToken = await WhaleToken.deploy();
+  const whaleToken = await WhaleToken.deploy(
+    'Lets Save The Wales Token',
+    'LSTWT',
+    'https://token.thisisnotacommercial.com/',
+  );
 
   console.log('Whale Token deployed to:', whaleToken.address);
 }
