ca02c8f262e324b5936e8765f6d289ffdec24876
Simon Emanuel Schmid <simon@unchained.shop>
2021-02-20 10:13:49 +0000

    Send ETH task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@ import 'hardhat-deploy-ethers';
 import 'hardhat-deploy';
 import 'hardhat-typechain';
 import '@typechain/ethers-v5';
+import { sign } from 'crypto';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -15,6 +16,16 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
     console.log(await account.address);
   }
 });
+
+task('send', 'Sends some ETH')
+  .addPositionalParam('to', 'The account you want to send ETH to')
+  .setAction(async ({ to }, { ethers }) => {
+    const [signer] = await ethers.getSigners();
+    await signer.sendTransaction({
+      to,
+      value: ethers.utils.parseEther('1.0'),
+    });
+  });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
