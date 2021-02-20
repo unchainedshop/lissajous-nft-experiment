15947879ac7b16574309262b77e06a45485e4e53
Simon Emanuel Schmid <simon@unchained.shop>
2021-02-20 11:50:16 +0000

    Mint task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,9 @@ import 'hardhat-deploy-ethers';
 import 'hardhat-deploy';
 import 'hardhat-typechain';
 import '@typechain/ethers-v5';
-import { sign } from 'crypto';
+
+import addresses from './addresses.json';
+import { WhaleToken } from './generated/typechain';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -25,6 +27,20 @@ task('send', 'Sends some ETH')
       to,
       value: ethers.utils.parseEther('1.0'),
     });
+  });
+
+task('mint', 'Mint a token for someone')
+  .addPositionalParam('to', 'The account you want to send the LSTWT to')
+  .setAction(async ({ to }, { ethers }) => {
+    const [signer] = await ethers.getSigners();
+    const { chainId } = await ethers.provider.getNetwork();
+    const whaleToken = (await ethers.getContractAt(
+      'WhaleToken',
+      addresses[chainId].WhaleToken,
+      signer,
+    )) as WhaleToken;
+
+    await whaleToken.mint(to);
   });
 
 // You need to export an object to set up your config
