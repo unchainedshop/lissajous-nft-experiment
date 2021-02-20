b67f9128cbd39e81fe8632da71638df00f3f9b9b
Simon Emanuel Schmid <simon@unchained.shop>
2021-02-20 13:50:58 +0000

    PoC write from contract

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+import addresses from '../addresses.json';
+// import WhalteTokenABI from '../artifacts/contracts/WhaleToken.sol/WhaleToken.json';
+import { WhaleToken__factory } from '../artifacts/typechain';
+
 const Index = () => {
-  return <h1>Hello</h1>;
+  const [account, setAccount] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const provider = new ethers.providers.Web3Provider(
+        (window as any).ethereum,
+      );
+      const { chainId, name } = await provider.getNetwork();
+
+      const blockNumber = await provider.getBlockNumber();
+
+      console.log({ blockNumber, addresses, name, chainId });
+
+      // const whaleToken: WhaleToken = new ethers.Contract(
+      //   addresses[chainId].WhaleToken,
+      //   WhalteTokenABI as any,
+      //   provider,
+      // );
+
+      const whaleToken = WhaleToken__factory.connect(
+        addresses[chainId].WhaleToken,
+        provider,
+      );
+
+      const baseUri = await whaleToken.baseURI();
+      console.log({ baseUri });
+
+      setAccount(baseUri);
+    })();
+  }, []);
+
+  return <h1>Hello {account}</h1>;
 };
 
 export default Index;
