96eba444b9dcae8e06d06e280bf3ab31d4058af1
Simon Emanuel Schmid <simon@unchained.shop>
2021-02-20 09:04:54 +0000

    Add eslint/prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,15 @@
-import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
+import { task } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-deploy-ethers';
+import 'hardhat-deploy';
+import '@symfoni/hardhat-react';
+import 'hardhat-typechain';
+import '@typechain/ethers-v5';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -15,5 +21,21 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 export default {
-  solidity: "0.7.3",
+  solidity: '0.7.3',
+  react: {
+    providerPriority: ['web3modal', 'hardhat'],
+  },
+  paths: {
+    react: './generated/hardhat',
+    deployments: './generated/deployments/',
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  typechain: {
+    outDir: './generated/typechain',
+    target: 'ethers-v5',
+  },
 };
