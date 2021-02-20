7a5d29fd3c311b6114e98a34b6d69af8d2668c48
Simon Emanuel Schmid <simon@unchained.shop>
2021-02-20 11:28:26 +0000

    Write address after deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,24 @@
+import fs from 'fs';
+
 async function main() {
   const WhaleToken = await global.ethers.getContractFactory('WhaleToken');
+  const { chainId } = await global.ethers.provider.getNetwork();
   const whaleToken = await WhaleToken.deploy(
     'Lets Save The Wales Token',
     'LSTWT',
     'https://token.thisisnotacommercial.com/',
   );
+
+  const addresses = fs.readFileSync(`./addresses.json`);
+
+  const newAddresses = {
+    ...JSON.parse(addresses),
+    [chainId]: {
+      WhaleToken: whaleToken.address,
+    },
+  };
+
+  fs.writeFileSync(`./addresses.json`, JSON.stringify(newAddresses, null, 2));
 
   console.log('Whale Token deployed to:', whaleToken.address);
 }
