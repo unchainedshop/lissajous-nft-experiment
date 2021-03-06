9f2c52f37ee722629b9bcb5c2c63168f4a8def02
Simon Emanuel Schmid <simon@unchained.shop>
2021-03-06 12:14:23 +0000

    Write tests for starblock

--- contracts/test/sample-test.ts
+++ contracts/test/sample-test.ts
@@ -3,6 +3,8 @@ import { ethers } from 'hardhat';
 import { LissajousToken } from '../artifacts/typechain';
 
 describe('LissajousToken', function () {
+  let token: LissajousToken;
+
   it('Deploy', async function () {
     const LissajousTokenContract = await ethers.getContractFactory(
       'LissajousToken',
@@ -11,12 +13,20 @@ describe('LissajousToken', function () {
       'Lissajous Token',
       'LISSA',
       'https://lissajous.art/api/token',
+      5,
+      20,
+      5,
     );
 
-    const token = ((await tx.deployed()) as any) as LissajousToken;
+    token = ((await tx.deployed()) as any) as LissajousToken;
 
     expect(await token.name()).to.equal('Lissajous Token');
     expect((await token.totalSupply()).toString()).to.equal('0');
+  });
+
+  it('Mint a token before starting block should fail', async () => {
+    await token.mint();
+    expect((await token.totalSupply()).toString()).to.equal('1');
   });
 
   it.skip('Owner can withdraw ether', () => {});
