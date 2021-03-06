e786ce47732f77d05572dc0227c859d2b7783603
Simon Emanuel Schmid <simon@unchained.shop>
2021-03-06 18:02:41 +0000

    Add token color from value

--- contracts/contracts/LissajousToken.sol
+++ contracts/contracts/LissajousToken.sol
@@ -49,7 +49,7 @@ contract LissajousToken is Context, Ownable, ERC721Pausable {
     mapping(uint256 => uint256) private _mintBlocks;
 
     // save the value provided when a token is minted
-    mapping(uint256 => uint256) private _mintValue;
+    mapping(uint256 => uint256) private _mintValues;
 
     constructor(
         string memory name,
@@ -80,17 +80,59 @@ contract LissajousToken is Context, Ownable, ERC721Pausable {
         // We cannot just use balanceOf to create the new tokenId because tokens
         // can be burned (destroyed), so we need a separate counter.
         _mint(msg.sender, _tokenIdTracker.current());
-        _mintValue[_tokenIdTracker.current()] = msg.value;
+        _mintValues[_tokenIdTracker.current()] = msg.value;
         _mintBlocks[_tokenIdTracker.current()] = block.number;
         _tokenIdTracker.increment();
     }
 
     function tokenMintValue(uint256 tokenId) public view returns (uint256) {
-        return _mintValue[tokenId];
+        return _mintValues[tokenId];
     }
 
     function tokenMintBlock(uint256 tokenId) public view returns (uint256) {
         return _mintBlocks[tokenId];
+    }
+
+    function tokenMintBlockHash(uint256 tokenId) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(_mintBlocks[tokenId]));
+    }
+
+    function tokenColor(uint256 tokenId) public view returns (bytes3) {
+        uint256 mintValue = tokenMintValue(tokenId);
+
+        if (mintValue >= 100 ether) {
+            return 0xffd700; // Gold
+        } else if (mintValue >= 70 ether) {
+            return 0x55FF55; // light_green
+        } else if (mintValue >= 30 ether) {
+            return 0xFFFF55; // yellow
+        } else if (mintValue >= 10 ether) {
+            return 0xFF55FF; // light_magenta
+        } else if (mintValue >= 7 ether) {
+            return 0x55FFFF; // light_cyan
+        } else if (mintValue >= 3 ether) {
+            return 0xFF5555; // light_red
+        } else if (mintValue >= 1 ether) {
+            return 0xFF5555; // ligth_blue
+        } else if (mintValue >= 0.4 ether) {
+            return 0xFFFFFF; // white
+        } else if (mintValue >= 0.4 ether) {
+            return 0xAAAAAA; // light_gray
+        } else if (mintValue >= 0.2 ether) {
+            return 0x00AA00; // green
+        } else if (mintValue >= 0.1 ether) {
+            return 0xAA5500; // brown
+        } else if (mintValue >= 0.07 ether) {
+            return 0xAA00AA; // magenta
+        } else if (mintValue >= 0.04 ether) {
+            return 0x00AAAA; // cyan
+        } else if (mintValue >= 0.02 ether) {
+            return 0x0000AA; // blue
+        } else if (mintValue >= 0.01 ether) {
+            return 0xAA0000; // red
+        }
+
+        return 0x555555; // dark_grey
     }
 
     function withdraw() public onlyOwner {
