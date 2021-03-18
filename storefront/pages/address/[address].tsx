a68728b1c49217f4ee27a054a2ee6a156c6a2bac
Simon Emanuel Schmid <simon@unchained.shop>
2021-03-18 14:59:47 +0000

    Blub

--- storefront/pages/address/[address].tsx
+++ storefront/pages/address/[address].tsx
@@ -69,14 +69,10 @@ const Address = () => {
     };
   }, [transactions.length]);
 
-  console.log(transactions);
-
   return (
     <div>
       <div className="text-center">
-        <h1>
-          {isOwner ? 'Your Tokens' : `Tokens of ${router.query.address}`}
-        </h1>
+        <h1>{isOwner ? 'Your Tokens' : `Tokens of ${router.query.address}`}</h1>
         {isOwner && transactions.length > 0 && (
           <>
             <h2>Pending Mints</h2>
@@ -90,47 +86,83 @@ const Address = () => {
                       key={i}
                       style={{ color: colorFromPrice(price) }}
                     >
-                      <svg width="45" height="45" viewBox="0 0 45 45" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
-                        <g fill="none" fill-rule="evenodd" transform="translate(1 1)" stroke-width="2">
-                            <circle cx="22" cy="22" r="6" stroke-opacity="0">
-                                <animate attributeName="r"
-                                    begin="1.5s" dur="3s"
-                                    values="6;22"
-                                    calcMode="linear"
-                                    repeatCount="indefinite" />
-                                <animate attributeName="stroke-opacity"
-                                    begin="1.5s" dur="3s"
-                                    values="1;0" calcMode="linear"
-                                    repeatCount="indefinite" />
-                                <animate attributeName="stroke-width"
-                                    begin="1.5s" dur="3s"
-                                    values="2;0" calcMode="linear"
-                                    repeatCount="indefinite" />
-                            </circle>
-                            <circle cx="22" cy="22" r="6" stroke-opacity="0">
-                                <animate attributeName="r"
-                                    begin="3s" dur="3s"
-                                    values="6;22"
-                                    calcMode="linear"
-                                    repeatCount="indefinite" />
-                                <animate attributeName="stroke-opacity"
-                                    begin="3s" dur="3s"
-                                    values="1;0" calcMode="linear"
-                                    repeatCount="indefinite" />
-                                <animate attributeName="stroke-width"
-                                    begin="3s" dur="3s"
-                                    values="2;0" calcMode="linear"
-                                    repeatCount="indefinite" />
-                            </circle>
-                            <circle cx="22" cy="22" r="8">
-                                <animate attributeName="r"
-                                    begin="0s" dur="1.5s"
-                                    values="6;1;2;3;4;5;6"
-                                    calcMode="linear"
-                                    repeatCount="indefinite" />
-                            </circle>
+                      <svg
+                        width="45"
+                        height="45"
+                        viewBox="0 0 45 45"
+                        xmlns="http://www.w3.org/2000/svg"
+                        stroke="#fff"
+                      >
+                        <g
+                          fill="none"
+                          fillRule="evenodd"
+                          transform="translate(1 1)"
+                          strokeWidth="2"
+                        >
+                          <circle cx="22" cy="22" r="6" strokeOpacity="0">
+                            <animate
+                              attributeName="r"
+                              begin="1.5s"
+                              dur="3s"
+                              values="6;22"
+                              calcMode="linear"
+                              repeatCount="indefinite"
+                            />
+                            <animate
+                              attributeName="stroke-opacity"
+                              begin="1.5s"
+                              dur="3s"
+                              values="1;0"
+                              calcMode="linear"
+                              repeatCount="indefinite"
+                            />
+                            <animate
+                              attributeName="stroke-width"
+                              begin="1.5s"
+                              dur="3s"
+                              values="2;0"
+                              calcMode="linear"
+                              repeatCount="indefinite"
+                            />
+                          </circle>
+                          <circle cx="22" cy="22" r="6" strokeOpacity="0">
+                            <animate
+                              attributeName="r"
+                              begin="3s"
+                              dur="3s"
+                              values="6;22"
+                              calcMode="linear"
+                              repeatCount="indefinite"
+                            />
+                            <animate
+                              attributeName="stroke-opacity"
+                              begin="3s"
+                              dur="3s"
+                              values="1;0"
+                              calcMode="linear"
+                              repeatCount="indefinite"
+                            />
+                            <animate
+                              attributeName="stroke-width"
+                              begin="3s"
+                              dur="3s"
+                              values="2;0"
+                              calcMode="linear"
+                              repeatCount="indefinite"
+                            />
+                          </circle>
+                          <circle cx="22" cy="22" r="8">
+                            <animate
+                              attributeName="r"
+                              begin="0s"
+                              dur="1.5s"
+                              values="6;1;2;3;4;5;6"
+                              calcMode="linear"
+                              repeatCount="indefinite"
+                            />
+                          </circle>
                         </g>
-                    </svg>
+                      </svg>
                     </div>
                   )),
               )}
